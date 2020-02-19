1c0eeb4938ec086d75d44cc89d07727ff3f7c047
khvalkoilya <khvalko.ilya@gmail.com>
2020-02-19 23:10:28 +0300

    feat: add solve

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 
 exports.min = function min (array) {
-  return 0;
+  return arguments.length==0 || array.length==0 ?0:array.sort((a,b)=>(a-b))[0] ;
 }
 
 exports.max = function max (array) {
-  return 0;
+    return arguments.length==0 || array.length==0 ?0:array.sort((a,b)=>(a-b))[array.length-1];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    return arguments.length==0 || array.length==0 ?0:array.reduce(function(sum,item){ return sum+=item;},0)/array.length;
 }
+
+
